8e09296aacf7004e1a0661b45b7d8f5e7637b573
JungWon Park <firstman9@protonmail.com>
2023-06-24 07:12:25 +0900

    1단계

--- app/page.tsx
+++ app/page.tsx
@@ -7,49 +7,7 @@ import { useState } from 'react'
 
 export default function Home() {
 
-  type Task = {
-    id: number,
-    name: string,
-    completed: boolean
-  }
-
-  const [task, setTask] = useState<Task>({
-    id: 0,
-    name: '',
-    completed: false
-  })
-
-  const [tasks, setTasks] = useState<Task[]>([])
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('Input Value:', event.target.value);
-    console.log('Tasks:', tasks);
-  
-    const inputValue = event.target.value.trim();
-    console.log('Trimmed Input Value:', inputValue);
-  
-    if (inputValue !== '') {
-      setTask({ ...task, name: inputValue });
-    }
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter' && task.name.trim() !== '') {
-      event.preventDefault();
-      setTasks(prevTasks => [...prevTasks, task]);
-      setTask(prevTask => ({
-        id: prevTask.id + 1,
-        name: '',
-        completed: false
-      }));
-    }
-  };
-  
-
-  const handleTaskClick = (id: number) => {
-    setTasks(tasks.map(task => task.id === id ? { ...task, completed: !task.completed } : task))
-  }
-
+ 
   return (
     <div className="container">
       <Head>
@@ -61,7 +19,7 @@ export default function Home() {
       <div className="wrapper">
         <div className="task-input">
           <Image src="bars-icon.svg" alt="icon" width={20} height={20} />
-          <input type="text" placeholder="Add a new task" value={task.name} onChange={handleInputChange} onKeyUp={handleKeyDown} />
+          <input type="text" placeholder="Add a new task" value=""/>
         </div>
         <div className="controls">
           <div className="filters">
@@ -72,11 +30,7 @@ export default function Home() {
           <button className="clear-btn">Clear All</button>
         </div>
         <ul className="task-box">
-          {tasks.map((task) => (
-            <li key={task.id} onClick={() => handleTaskClick(task.id)}>
-              <p className={task.completed ? 'checked' : ''}>{task.name}</p>
-            </li>
-          ))}
+          <li><p>1111</p></li>
         </ul>
       </div>
     </div>
